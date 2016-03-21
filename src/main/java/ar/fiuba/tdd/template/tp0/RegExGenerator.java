4c4bb9c404d3b6c57fe8880a31b6c0f65c1b3a9a
maxipe <maxipenia@hotmail.com>
2016-03-21 09:27:36 -0300

    Implemented RegExGenerator and new tests created.

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -4,21 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RegExGenerator {
-    // TODO: Uncomment this field
-    //private int maxLength;
+    private int maxLength;
 
-    //public RegExGenerator(int maxLength) {
-    //    this.maxLength = maxLength;
-    //}
+    public RegExGenerator(int maxLength) {
+        this.maxLength = maxLength;
+    }
 
-    // TODO: Uncomment parameters
-    public List<String> generate(/*String regEx, int numberOfResults*/) {
-        return new ArrayList<String>() {
-            {
-                add("a");
-                add("b");
-                add("c");
-            }
-        };
+    public List<String> generate(String regEx, int numberOfResults) {
+        if (maxLength <= 0) {
+            return null;
+        }
+        ArrayList<String> strings = new ArrayList<String>();
+
+        Parser parser = new Parser(regEx);
+        parser.parse();
+        ArrayList<Expression> expressions = parser.getExpressions();
+
+        for (int number = 0; number < numberOfResults; number++) {
+            StringBuilder string = new StringBuilder();
+            expressions.forEach((expression) -> {
+                    string.append(expression.getString());
+                }
+            );
+            strings.add(string.toString());
+        }
+
+        return strings;
     }
 }
--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -2,18 +2,20 @@ package ar.fiuba.tdd.template.tp0;
 
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class RegExGeneratorTest {
+    private static final int MAX_LENGTH = 50;
 
     private boolean validate(String regEx, int numberOfResults) {
-        RegExGenerator generator = new RegExGenerator();
-        // TODO: Uncomment parameters
-        List<String> results = generator.generate(/*regEx, numberOfResults*/);
+        RegExGenerator generator = new RegExGenerator(MAX_LENGTH);
+
+        List<String> results = generator.generate(regEx, numberOfResults);
         // force matching the beginning and the end of the strings
         Pattern pattern = Pattern.compile("^" + regEx + "$");
         return results
@@ -61,5 +63,45 @@ public class RegExGeneratorTest {
         assertTrue(validate("[abc]+", 1));
     }
 
-    // TODO: Add more tests!!!
+    @Test
+    public void testMixedExpression() {
+        assertTrue(validate("[abc]+gk?j.+p*[aqw]+", 1));
+    }
+
+    private boolean differentResults(String regEx, int numberOfResults) {
+        RegExGenerator generator = new RegExGenerator(MAX_LENGTH);
+        List<String> results = generator.generate(regEx, numberOfResults);
+        java.util.Set<String> uniqueResults = new HashSet<>(results);
+        return results.size() == uniqueResults.size();
+    }
+
+    @Test
+    public void testAnyCharacterDifferentResults() {
+        assertTrue(differentResults(".", 20));
+    }
+
+    @Test
+    public void testZeroOrOneDifferentResults() {
+        assertTrue(differentResults("a?", 2));
+    }
+
+    @Test
+    public void testZeroOrOneMoreResultsThanPosible() {
+        assertFalse(differentResults("a?", 3));
+    }
+
+    @Test
+    public void testZeroOrManyDifferentResults() {
+        assertTrue(differentResults("a*", 5));
+    }
+
+    @Test
+    public void testOneOrManyDifferentResults() {
+        assertTrue(differentResults("a+", 5));
+    }
+
+    @Test
+    public void testComplexRegExDifferentResults() {
+        assertTrue(differentResults("a*q+[lat]?f\\l\\\\+w?.q", 60));
+    }
 }
